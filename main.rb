de6af8dc50feebaa388c7ffa432454fa4b539b4e
takatoshi.tenko <chanten811@gmail.com>
2024-05-04 20:29:02 +0900

    カスタム関数作成

---
+++ main.rb
@@ -0,0 +1,59 @@
+# puts "Hello, Ruby.", "へっぉw"
+# puts(10)
+# =begin
+# aaaa
+# =end
+
+# def hello
+#   puts "Hello, Ruby."
+# end
+
+# hello
+
+# a = 20
+# if a >= 10 then
+#   print "greater\n"
+# end
+# if a <= 9 then
+#   print "smaller"
+# end
+
+# # i = 1
+# # while i <= 10 
+# #   print i, "\n"
+# #   i = i + 1
+# # end
+
+# 3.times do
+#   print "Hello, Ruby.\n"
+# end
+
+# names = ["小林", "林", "高野", "盛岡"]
+
+# # puts names[0]
+# # puts names.size
+
+# names.each do |name|
+#   puts name
+# end
+
+# address = {name: "髙橋", furigana: "タカハシ"}
+# address[:tel] = "1234567890"
+# puts address
+
+# address.each do |key, value|
+#   puts "#{key}: #{value}"
+# end
+
+# names.each do |name|
+#   if /林/ =~ name
+#     puts name
+#   end
+# end
+
+def hello 
+  puts "Hello, Ruby."
+end
+
+hello
+
