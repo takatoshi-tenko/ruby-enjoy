6913d897c2b188ebd92b5c025e267f3e76fb668c
takatoshi.tenko <chanten811@gmail.com>
2024-05-04 22:43:49 +0900

    misc

--- main.rb
+++ main.rb
@@ -62,3 +62,19 @@
 # days = Date.today - Date.new(1993, 2, 24)
 # puts(days.to_i)
 
+class HelloWorld
+  def initialize(myname = "Ruby")
+    @name = myname
+  end
+
+  def hello
+    puts "Hello, world. I am #{@name}"
+  end
+end
+
+bob = HelloWorld.new("Bob")
+alice = HelloWorld.new("Alice")
+ruby = HelloWorld.new
+bob.hello
+alice.hello
+ruby.hello
